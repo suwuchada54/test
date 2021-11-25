efd5a8ec15378e8742d8acf28c7989b889c8e5c9
suwuchada54 <95045750+suwuchada54@users.noreply.github.com>
2021-11-26 00:18:19 +0700

    Add files via upload

---
+++ 2Ex_PAE.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N=10;
+    double x[N]={1,2,3};
+    int i=0;
+    while (i<N)
+    {
+    cout<<"N ="<<i<<x[i]<<"\n";
+    i++;
+    
+    }
+    return 0;
+}
